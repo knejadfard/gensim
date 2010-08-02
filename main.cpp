a5560863be07378b87048a6a928a94fdc5e35253
Anarion <kinganarion@hotmail.com>
2010-08-02 13:24:50 +0430

    Removed test lines

--- main.cpp
+++ main.cpp
@@ -11,19 +11,8 @@ int main() {
     try {
         population p(5);
         p.print();
-        cout<<"--- After Crossingover ---"<<endl;
-        /*p.evolve(20);
-        p.print();*/
-        // TEST
-        member m1("12345");
-        cout<<"<"<<m1.get_phenotype()<<"> to <";
-        string::iterator it1, it2;
-        m1.random_range(it1, it2);
-        string tmp = m1.get_phenotype();
-        //iter_swap(it1, it2);
-        swap_ranges(it1, it2, tmp.begin());
-        cout<<tmp<<">"<<endl;
-        //
+        p.evolve(20);
+        p.print();
     } catch(exception& e) {
         cout<<e.what()<<endl;
     }
