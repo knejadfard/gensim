78644624bcf940a54b0200d8412306617dea88d2
Anarion <kinganarion@hotmail.com>
2010-08-02 19:19:43 +0430

    Modified program's interface to be more flexible

--- main.cpp
+++ main.cpp
@@ -1,16 +1,25 @@
 #include <iostream>
 #include <exception>
+#include <sstream>
+#include <memory>
 
 #include "member.hpp"
 #include "population.hpp"
 
 using namespace std;
 
-int main() {
+int main(int argc, char **argv) {
     try {
-        population p(member("genetics"), 10);
+        //Changed to dynamic allocation to be able to release before heavy operation
+        auto_ptr<stringstream> i = auto_ptr<stringstream>(new stringstream);
+        (*i)<<argv[2]<<" "<<argv[3];
+        int cycles, size;
+        (*i)>>size>>cycles;
+        i.reset(); //destruct the allocated memory
+        //
+        population p(member(argv[1]), size);
         p.print();
-        p.evolve(100000);
+        p.evolve(cycles);
         p.print();
         cout<<"Size: "<<p.size()<<endl;
     } catch(exception& e) {
