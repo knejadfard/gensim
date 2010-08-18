5c9a2e2b79d5cefaf02a2413e707f90f6dde2005
Anarion <kinganarion@hotmail.com>
2010-08-18 09:56:22 +0430

    Wrote basic chphenotype class

--- chphenotype.hpp
+++ chphenotype.hpp
@@ -1,6 +1,15 @@
 #ifndef _CHPHENOTYPE_
 #define _CHPHENOTYPE_
 
+#include <cstdlib>
 
+class chphenotype {
+public:
+    char random(); //generate a random character between 'a' and 'z'
+};
+
+static inline char chphenotype::random() {
+    return rand()%('z'-'a'+1) + 'a';
+}
 
 #endif
