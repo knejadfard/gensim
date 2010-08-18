f03a7360f14d1117cceeb0fa717a45424de601ff
Anarion <kinganarion@hotmail.com>
2010-08-18 14:33:47 +0430

    member.hpp-> Added operator+ and a new constructor, also removed some commented code

--- member.hpp
+++ member.hpp
@@ -12,84 +12,60 @@
 
 //typedef std::string::iterator siterator;
 
-//ADD CROSSOVER
-class member {
-    phenotype<char, 10, chphenotype> phen;
+template<class P> class member {
+    P phen;
 public:
     //void random_range(siterator& it1, siterator& it2);
-    member(); //generates random phenotype
-    //member(const std::string& phenotype);
-    member(const member& rhs); //copies phenotype of another member
-    member(const phenotype<char, 10, chphenotype>& data);
-    member& operator=(const member& rhs); //copies phenotype of another member
-    //size_t calculate_fitness(const member& alpha) const; //calculate fitness based on the alpha
+    member(); //initialize member with random phenotype
+    member(const member& rhs);
+    member(const P& data); //initialize member from a given phenotype, should stay implicit
+    member& operator=(const member& rhs); //self-wise
     int fitness(const member& alpha);
-    //void mutate(const size_t& max_percent); //randomly apply mutations to phenotype
     //std::string first_half() const; //return phenotype's first half
     //std::string second_half() const; //return phenotype's second half
-    //member generate(const size_t& size) const; //generate a random member
-    member mate(const member& rhs) const;
-    //member breed(const member& rhs) const;
-    //void crossover(member& rhs); //MODIFY TO APPLY RANDOM RANGE CROSS
-    const phenotype<char, 10, chphenotype>& get_phenotype() const;
-    phenotype<char, 10, chphenotype>& get_phenotype();
-    void set_phenotype(const phenotype<char, 10, chphenotype>& pheno);
-    //unsigned int fitness();
-    //bool operator<(const member& rhs) const; //compare fitness
+    //member mate(const member& rhs) const;
+    const P& get_phenotype() const;
+    P& get_phenotype();
+    void set_phenotype(const P& pheno);
+    member<P> operator+(const member<P>& rhs) const; //generate a newborn
 };
-
-//inline member::member(const std::string& phenotype): phen(phenotype) {}
 
 inline member::member(): phen() {}
 
-inline member::member(const member& rhs): phen() {
-    //srand(time(0));
+template<class P> inline member<P>::member(const member<P>& rhs): phen() {
     operator=(rhs);
 }
 
-inline member::member(const phenotype<char, 10, chphenotype>& data): phen(data) {}
+template<class P> inline member<P>::member(const P& data): phen(data) {}
 
-inline const phenotype<char, 10, chphenotype>& member::get_phenotype() const {
+template<class P> inline const P& member<P>::get_phenotype() const {
     return phen;
 }
 
-inline phenotype<char, 10, chphenotype>& member::get_phenotype() {
+template<class P> inline P& member<P>::get_phenotype() {
     return phen;
 }
 
-/*inline unsigned int member::fitness() {
-    return ft;
-}*/
-
-/*inline char member::random_char(const char& a, const char& b) const {
-    return rand()%(b-a+1) + a;
-}*/
-
-/*size_t member::calculate_fitness(const member& alpha) const {
-    //if(alpha.phen.size() != phen.size())
-        //throw exception
-        size_t ft = 0; //must become 0 when recalculating fitness
-        for(size_t i=0; i<phen.size(); i++) {
-            ft += abs(phen.at(i)-alpha.phen.at(i));
-        }
-        return ft;
-}*/
-
-inline int member::fitness(const member& alpha) const {
+template<class P> inline int member<P>::fitness(const member<P>& alpha) const {
     return phen.fitness(alpha.get_phenotype());
 }
 
-/*inline void member::mutate(const size_t& max_percent) {
-    size_t mutate_count = floor( float(rand()%max_percent)/100*phen.size() );
-    for(size_t i=0; i<mutate_count; i++) {
-        phen.at( rand()%phen.size() ) = random_char('a', 'z'); //assign random value to random index
-    }
-}*/
-
-inline member& member::operator=(const member& rhs) {
+template<class P> inline member<P>& member<P>::operator=(const member<P>& rhs) {
     if(this==&rhs) return *this;
     phen = rhs.get_phenotype();
     return *this;
+}
+
+template<class P> inline void member<P>::set_phenotype(const P& pheno) {
+    phen = pheno;
+}
+
+/*template<class P> inline member<P> member<P>::mate(const member<P>& rhs) const {
+    return phen + rhs.get_phenotype();
+}*/
+
+template<class P> inline member<P> member<P>::operator+(const member<P>& rhs) const {
+    return phen + rhs.get_phenotype();
 }
 
 /*member member::generate(const size_t& size) const {
@@ -103,10 +79,6 @@ inline member& member::operator=(const member& rhs) {
 /*inline bool member::operator<(const member& rhs) const {
     return calculate_fitness()<rhs.calculate_fitness();
 }*/
-
-inline void member::set_phenotype(const phenotype<char, 10, chphenotype>& pheno) {
-    phen = pheno;
-}
 
 /*void member::crossover(member& rhs) {
     //siterator it1=rhs.get_phenotype().begin(), it2=it1+5;
@@ -133,10 +105,6 @@ std::string member::second_half() const {
     }
     return tmp;
 }*/
-
-inline member member::mate(const member& rhs) const {
-    return phen.mix(rhs.get_phenotype());
-}
 
 /*void member::random_range(siterator& it1, siterator& it2) {
     it1 = phen.begin();
