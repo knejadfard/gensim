fa56c5affe5f3fef767212c2a8a9ab32ad2122b0
Anarion <kinganarion@hotmail.com>
2010-08-18 11:25:15 +0430

    Fixed problem with static

--- chphenotype.hpp
+++ chphenotype.hpp
@@ -5,10 +5,10 @@
 
 class chphenotype {
 public:
-    char random(); //generate a random character between 'a' and 'z'
+    static char random(); //generate a random character between 'a' and 'z'
 };
 
-static inline char chphenotype::random() {
+inline char chphenotype::random() {
     return rand()%('z'-'a'+1) + 'a';
 }
 
