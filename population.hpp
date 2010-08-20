97c49d0642c4ee90c930c1668d7fe76480b199df
Anarion <kinganarion@hotmail.com>
2010-08-20 17:36:10 +0430

    population: Changed to template for improved generic programming

--- population.hpp
+++ population.hpp
@@ -7,12 +7,10 @@
 #include <cmath>
 
 #include "member.hpp"
-#include "phenotype.hpp"
-#include "chphenotype.hpp"
 
-class population {
-    typedef member< phenotype<char, 10, chphenotype> > member_t;
-    typedef std::vector<member_t>::iterator miterator;
+template <class Phenotype> class population {
+    typedef member< Phenotype > member_t;
+    typedef typename std::vector<member_t>::iterator miterator;
     std::vector<member_t> pvec;
     member_t a;
     void advance();
@@ -33,19 +31,16 @@ public:
     size_t size() const;
 };
 
-population::population(const member_t& alpha, const size_t& size): pvec(), a(alpha), cmp(alpha) {
-    //generate random population
+template <class Phenotype> population<Phenotype>::population(const member_t& alpha, const size_t& size): pvec(), a(alpha), cmp(alpha) {
     srand(time(0));
     for(size_t i=0; i<size; i++) {
         pvec.push_back(member_t());
     }
 }
-
-size_t population::size() const{
+template <class Phenotype> size_t population<Phenotype>::size() const{
     return pvec.size();
 }
-
-void population::evolve(const size_t& cycles) {
+template <class Phenotype> void population<Phenotype>::evolve(const size_t& cycles) {
     for(size_t i=0; i<cycles; i++) {
         /*for(miterator it1=pvec.begin(); it1!=pvec.end(); it1++) {
             //it1->crossover(*it2);
@@ -58,19 +53,12 @@ void population::evolve(const size_t& cycles) {
     }
     std::cout<<std::endl;
 }
-
-void population::print() {
+template <class Phenotype> void population<Phenotype>::print() {
     for(miterator it=pvec.begin(); it!=pvec.end(); it++) {
         std::cout<<"Phenotype: <"<<it->get_phenotype()<<"> Fitness: <"<<it->get_phenotype().fitness(a.get_phenotype())<<">"<<std::endl;
     }
 }
-
-void population::advance() {
-    //remove last quarter, must be sorted
-    //int count = ceil(float(pvec.size())/4);
-    /*for(int i=0; i<count; i++) {
-        pvec.pop_back();
-    }*/
+template <class Phenotype> void population<Phenotype>::advance() {
     std::vector<member_t> nvec;
     //add best quarter to new population
     /*for(int i=0; i<count; i++) {
@@ -78,7 +66,6 @@ void population::advance() {
     }*/
     nvec.push_back(pvec.at(0));
     nvec.push_back(pvec.at(1));
-
     nvec.push_back(pvec.at(0)+pvec.at(1));
     for(int i=2;i<pvec.size()-1; i++) {
         nvec.push_back(pvec.at(i)+pvec.at(i+1));
