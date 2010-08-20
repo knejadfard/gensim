c7280b33bfc53a71e88cafacf967f5bffaedb74c
Anarion <kinganarion@hotmail.com>
2010-08-20 17:46:05 +0430

    member: renamed template parameter

--- member.hpp
+++ member.hpp
@@ -7,51 +7,51 @@
 #include <ctime>
 #include <cmath>
 
-template<class P> class member {
-    P phen;
+template<class Phenotype> class member {
+    Phenotype phen;
 public:
     member(); //initialize member with random phenotype
     member(const member& rhs);
-    member(const P& data); //initialize member from a given phenotype, should stay implicit
+    member(const Phenotype& data); //initialize member from a given phenotype, should stay implicit
     member& operator=(const member& rhs); //self-wise
     int fitness(const member& alpha) const;
-    const P& get_phenotype() const;
-    P& get_phenotype();
-    void set_phenotype(const P& pheno);
-    member<P> operator+(const member<P>& rhs) const; //generate a newborn
+    const Phenotype& get_phenotype() const;
+    Phenotype& get_phenotype();
+    void set_phenotype(const Phenotype& pheno);
+    member<Phenotype> operator+(const member<Phenotype>& rhs) const; //generate a newborn
 };
 
-template<class P> inline member<P>::member(): phen() {}
+template<class Phenotype> inline member<Phenotype>::member(): phen() {}
 
-template<class P> inline member<P>::member(const member<P>& rhs): phen() {
+template<class Phenotype> inline member<Phenotype>::member(const member<Phenotype>& rhs): phen() {
     operator=(rhs);
 }
 
-template<class P> inline member<P>::member(const P& data): phen(data) {}
+template<class Phenotype> inline member<Phenotype>::member(const Phenotype& data): phen(data) {}
 
-template<class P> inline const P& member<P>::get_phenotype() const {
+template<class Phenotype> inline const Phenotype& member<Phenotype>::get_phenotype() const {
     return phen;
 }
 
-template<class P> inline P& member<P>::get_phenotype() {
+template<class Phenotype> inline Phenotype& member<Phenotype>::get_phenotype() {
     return phen;
 }
 
-template<class P> inline int member<P>::fitness(const member<P>& alpha) const {
+template<class Phenotype> inline int member<Phenotype>::fitness(const member<Phenotype>& alpha) const {
     return phen.fitness(alpha.get_phenotype());
 }
 
-template<class P> inline member<P>& member<P>::operator=(const member<P>& rhs) {
+template<class Phenotype> inline member<Phenotype>& member<Phenotype>::operator=(const member<Phenotype>& rhs) {
     if(this==&rhs) return *this;
     phen = rhs.get_phenotype();
     return *this;
 }
 
-template<class P> inline void member<P>::set_phenotype(const P& pheno) {
+template<class Phenotype> inline void member<Phenotype>::set_phenotype(const Phenotype& pheno) {
     phen = pheno;
 }
 
-template<class P> inline member<P> member<P>::operator+(const member<P>& rhs) const {
+template<class Phenotype> inline member<Phenotype> member<Phenotype>::operator+(const member<Phenotype>& rhs) const {
     return phen + rhs.get_phenotype();
 }
 
