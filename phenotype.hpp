ec03b13c9df6eeb456ec72579ee535d8973f5455
Anarion <kinganarion@hotmail.com>
2010-08-21 19:27:15 +0430

    phenotype: renamed Ttraits to Tpolicy

--- phenotype.hpp
+++ phenotype.hpp
@@ -7,58 +7,58 @@
 
 //RECONSIDER size_t?
 //CONSIDER USING TYPEDEF TO MAKE LIFE EASIER
-template<class T, size_t size, class Ttraits> class phenotype {
+template<class T, size_t size, class Tpolicy> class phenotype {
     T pdata[size]; //phenotype data
 public:
     phenotype(const T& a); //make phenotype using a single element, copied to fill pdata
     phenotype(); //make phenotype with random data
-    phenotype(const phenotype<T, size, Ttraits>& rhs);
+    phenotype(const phenotype<T, size, Tpolicy>& rhs);
     phenotype(const T data[size]);
-    phenotype<T, size, Ttraits>& operator=(const phenotype<T, size, Ttraits>& rhs);
-    int fitness(const phenotype<T, size, Ttraits>& rhs) const;
+    phenotype<T, size, Tpolicy>& operator=(const phenotype<T, size, Tpolicy>& rhs);
+    int fitness(const phenotype<T, size, Tpolicy>& rhs) const;
     char& operator[](const size_t& index);
     const char& operator[](const size_t& index) const;
     void mutate(const float& rate);
-    phenotype<T, size, Ttraits> operator+(const phenotype<T, size, Ttraits>& rhs) const;
+    phenotype<T, size, Tpolicy> operator+(const phenotype<T, size, Tpolicy>& rhs) const;
 };
 
-template<class T, size_t size, class Ttraits> inline phenotype<T, size, Ttraits>::phenotype(const phenotype<T, size, Ttraits>& rhs) {
+template<class T, size_t size, class Tpolicy> inline phenotype<T, size, Tpolicy>::phenotype(const phenotype<T, size, Tpolicy>& rhs) {
     operator=(rhs);
 }
 
-template<class T, size_t size, class Ttraits> phenotype<T, size, Ttraits>::phenotype(const T& a) {
+template<class T, size_t size, class Tpolicy> phenotype<T, size, Tpolicy>::phenotype(const T& a) {
     for(size_t i=0; i<size; i++) {
         pdata[i] = a;
     }
 }
 
-template<class T, size_t size, class Ttraits> phenotype<T, size, Ttraits>::phenotype() {
+template<class T, size_t size, class Tpolicy> phenotype<T, size, Tpolicy>::phenotype() {
     for(size_t i=0; i<size; i++) {
-        pdata[i] = Ttraits::random();
+        pdata[i] = Tpolicy::random();
     }
 }
 
 //NEEDS MORE RESTRICTION FOR DATA'S SIZE
-template<class T, size_t size, class Ttraits> phenotype<T, size, Ttraits>::phenotype(const T data[size]) {
+template<class T, size_t size, class Tpolicy> phenotype<T, size, Tpolicy>::phenotype(const T data[size]) {
     for(int i=0; i<size; i++) {
         pdata[i] = data[i];
     }
 }
 
-template<class T, size_t size, class Ttraits> inline char& phenotype<T, size, Ttraits>::operator[](const size_t& index) {
+template<class T, size_t size, class Tpolicy> inline char& phenotype<T, size, Tpolicy>::operator[](const size_t& index) {
     //CHECK BOUNDS
     //ADD EXCEPTION
     return pdata[index];
 }
 
-template<class T, size_t size, class Ttraits> inline const char& phenotype<T, size, Ttraits>::operator[](const size_t& index) const {
+template<class T, size_t size, class Tpolicy> inline const char& phenotype<T, size, Tpolicy>::operator[](const size_t& index) const {
     //CHECK BOUNDS
     //ADD EXCEPTION
     return pdata[index];
 }
 
 
-template<class T, size_t size, class Ttraits> int phenotype<T, size, Ttraits>::fitness(const phenotype<T, size, Ttraits>& rhs) const {
+template<class T, size_t size, class Tpolicy> int phenotype<T, size, Tpolicy>::fitness(const phenotype<T, size, Tpolicy>& rhs) const {
     size_t fitness = 0;
     for(int i=0; i<size; i++) {
         fitness += abs(rhs[i] - pdata[i]);
@@ -66,7 +66,7 @@ template<class T, size_t size, class Ttraits> int phenotype<T, size, Ttraits>::f
     return fitness;
 }
 
-template<class T, size_t size, class Ttraits> phenotype<T, size, Ttraits>& phenotype<T, size, Ttraits>::operator=(const phenotype<T, size, Ttraits>& rhs) {
+template<class T, size_t size, class Tpolicy> phenotype<T, size, Tpolicy>& phenotype<T, size, Tpolicy>::operator=(const phenotype<T, size, Tpolicy>& rhs) {
     if(this==&rhs) return *this;
     for(size_t i=0; i<size; i++) {
        pdata[i] = rhs[i];
@@ -74,15 +74,15 @@ template<class T, size_t size, class Ttraits> phenotype<T, size, Ttraits>& pheno
     return *this;
 }
 
-template<class T, size_t size, class Ttraits> void phenotype<T, size, Ttraits>::mutate(const float& rate) {
+template<class T, size_t size, class Tpolicy> void phenotype<T, size, Tpolicy>::mutate(const float& rate) {
     size_t mutate_count = floor(rate*(rand()%size+1));
     for(size_t i=0; i<mutate_count; i++) {
-        pdata[rand()%size] = Ttraits::random();
+        pdata[rand()%size] = Tpolicy::random();
     }
 }
 
 //MAY NEED IMPROVEMENT
-template<class T, size_t size, class Ttraits> phenotype<T, size, Ttraits> phenotype<T, size, Ttraits>::operator+(const phenotype<T, size, Ttraits>& rhs) const {
+template<class T, size_t size, class Tpolicy> phenotype<T, size, Tpolicy> phenotype<T, size, Tpolicy>::operator+(const phenotype<T, size, Tpolicy>& rhs) const {
     T tmp[size];
     int half = ceil((float)size/2);
     for(int i=0; i<half; i++) {
@@ -95,7 +95,7 @@ template<class T, size_t size, class Ttraits> phenotype<T, size, Ttraits> phenot
 }
 
 //global overload for output to ostream
-template<class T, size_t size, class Ttraits> std::ostream& operator<<(std::ostream& out, const phenotype<T, size, Ttraits>& rhs) {
+template<class T, size_t size, class Tpolicy> std::ostream& operator<<(std::ostream& out, const phenotype<T, size, Tpolicy>& rhs) {
     for(size_t i=0; i<size; i++) {
         out<<rhs[i];
     }
